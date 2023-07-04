5c74cb303fd86fa47f159f054f8ac5ac811d80d5
m09238yx <yifan.xu-9@postgrad.manchester.ac.uk>
2023-07-04 22:47:46 +0100

    update some roads

--- route3.pl
+++ route3.pl
@@ -11,11 +11,13 @@ node(5,friend(alex,emma), initial_fact, []).
 node(2,call(alex,emma), initial_fact, []).
 node(8,give_advice(forcast), initial_fact, []).
 node(16,app(forcast), initial_fact, []).
-node(13,green(forcast), initial_fact, []).
+node(13,smile(forcast), initial_fact, []).
 
-%node(14,route(c), initial_fact, []).
-node(14,light_traffic(c), initial_fact, []).
-node(14,connected(00,20), initial_fact, []).
+
+node(14,green(c), initial_fact, []).
+node(14,off_peak(c), initial_fact, []).
+node(14,connected(00,10), initial_fact, []).
+node(14,connected(10,20), initial_fact, []).
 node(14,connected(20,30), initial_fact, []).
 node(14,highway(30,40), initial_fact, []).
 node(14,route_name(c), initial_fact, []).
@@ -41,22 +43,25 @@ process_road(Road) :-
 rule(3,[friend(X,Z),call(X,Z),give_advice(W)],take_advice(X,W)).
 rule(6,[app(W),take_advice(X,W)],download(X,W)).
 
-rule(7,[green(W)],predict_sunny(W)).
+rule(7,[smile(W)],predict_sunny(W)).
 rule(9,[predict_sunny(W),download(X,W)],weather_sunny(X)).
 
 rule(16,[connected(X,Y)],road(X,Y)).
-rule(16,[connected(X,Z),road(Z,Y)],road(X,Y)).
+rule(16,[road(X,Z),road(Z,Y)],road(X,Y)).
 rule(16,[road(X,Y),road(Y,Z),highway(Z,P),route_name(C)],route(C)).
-%rule(16,[road(From, To),road(From, To)],route(C)).
 
-% Predicate to convert individual roads into a road_set
-%convert_roads(RoadSet) :-	
-  %findall(road(From, To), road(From, To), RoadSet).
 
+rule(7,[green(X),off_peak(X)],light_traffic(X)).
 
 rule(16,[light_traffic(C),weather_sunny(X),route(C),name(X)],valid_route(C)).
 
 rule(16,[name(X),route_set(A,B,C),not(valid_route(A)),not(valid_route(B)),valid_route(C)],chosen_route(X,C)).
 
 
+
+%rule(16,[road(From, To),road(From, To)],route(C)).
+
+% Predicate to convert individual roads into a road_set
+%convert_roads(RoadSet) :-	
+  %findall(road(From, To), road(From, To), RoadSet).
 
