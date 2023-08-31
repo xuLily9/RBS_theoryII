b75e705e270c97643a88291eb070d9b037684dd1
m09238yx <yifan.xu-9@postgrad.manchester.ac.uk>
2023-08-31 09:47:56 +0100

    success pretty print

--- print2.pl
+++ print2.pl
@@ -19,21 +19,14 @@ print_children([Child | Children], Indent, ChildrenCount, Rest) :-
     NewChildrenCount is ChildrenCount - 1,
     print_children(Children, Indent, NewChildrenCount, Rest).
 
-% 将您提供的树数据作为参数传递给print_tree/1
-%tree_data([node(44,valid_route(c),14,[node(26,light_traffic(c),initial_fact,[]),node(30,weather_sunny(alex),4,[node(27,predict_sunny(forcast),3,[node(7,smile(forcast),initial_fact,[])]),node(29,download(alex,forcast),2,[node(6,app(forcast),initial_fact,[]),node(28,take_advice(alex,forcast),1,[node(3,friend(alex,emma),initial_fact,[]),node(4,call(alex,emma),initial_fact,[]),node(5,give_advice(forcast),initial_fact,[])])])]),node(31,gas_station(0),13,[node(14,transport(car),initial_fact,[]),node(8,need_gas(car),initial_fact,[]),node(9,gas(0),initial_fact,[])]),node(12,airport(30),initial_fact,[]),node(43,find_route(0,30),9,[node(35,direct_route(0,10),6,[node(34,road(0,10),10,[node(31,gas_station(0),13,[node(14,transport(car),initial_fact,[]),node(8,need_gas(car),initial_fact,[]),node(9,gas(0),initial_fact,[])]),node(10,park(10),initial_fact,[]),node(33,not(congestion(10)),unprovable,[])])]),node(42,find_route(10,30),9,[node(38,direct_route(10,20),6,[node(37,road(10,20),11,[node(10,park(10),initial_fact,[]),node(11,school(20),initial_fact,[]),node(36,not(peak_hours(20)),unprovable,[])])]),node(41,find_route(20,30),8,[node(40,direct_route(20,30),6,[node(39,road(20,30),12,[node(11,school(20),initial_fact,[]),node(12,airport(30),initial_fact,[]),node(15,highway(20,30),initial_fact,[])])])])])]),node(1,name(alex),initial_fact,[])]),node(26,light_traffic(c),initial_fact,[])]).
 
-% 调用print_tree/1来打印树的结构
-%:- initialization(tree_data(Data), print_tree(Data), halt).
-
-
-% 列表转树形结构
 list_to_tree([], []).
 list_to_tree([H|T], [H|Children]) :-
-    is_list(H),  % 如果元素是列表，递归转换为子树
+    is_list(H),  %如果元素是列表递归转换为子树
     list_to_tree(H, Children),
-    list_to_tree(T, Rest).
+    list_to_tree(T, _Rest).
 list_to_tree([H|T], [H|Rest]) :-
-    \+ is_list(H),  % 如果元素不是列表，保留元素
+    \+ is_list(H),  %如果元素不是列表保留元素
     list_to_tree(T, Rest).
 
 print:-
