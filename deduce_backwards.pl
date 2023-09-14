a7d7c26a53d298ce9c060a30c1154f443d9c2e88
m09238yx <yifan.xu-9@postgrad.manchester.ac.uk>
2023-09-14 12:31:10 +0100

    update

--- deduce_backwards.pl
+++ deduce_backwards.pl
@@ -1,15 +1,29 @@
 :-dynamic node/4.
 
+
 deduce_backwards(Q, node(ID, Q, R , DAG)):-
    node(ID, Q, R , DAG).
 deduce_backwards(Q, node(ID, Q, ID_r , NodeList)):-
    rule(ID_r, A, Q),
    \+ node(_ID, Q, _ID_r, _NodeList),
+   write(A),
    check_antecedants(A, NodeList), !,
    countNumbers(Numbers),
    ID is Numbers +1,
    assert(node(ID,Q,ID_r,NodeList)).
- 
+
+%deduce_backwards(not(H), _):-
+%    deduce_backwards(H, _DAG), !.
+
+% Add a base case for non-existent facts
+%deduce_backwards(_, _).
+
+deduce_backwards(not(H), node(ID_n, not(H), DAG, [])):-
+    deduce_backwards(H, DAG), !,
+    countNumbers(Numbers),
+    ID_n is Numbers + 1,
+    assert(node(ID_n, not(H), DAG, [])).
+
 countNumbers(Numbers) :-
   aggregate_all(count, node(_,_,_,_), Numbers).
 
@@ -21,11 +35,13 @@ check_antecedants([H|T], [node(ID, H, R, DAG)|NodeList]):-
 
 check_antecedants([not(H)|T], [node(ID_n,not(H),unprovable,[])|NodeList]):-
     \+ deduce_backwards(H, _DAG), !,
-    %deduce_backwards(H, _DAG), !,
     countNumbers(Numbers),
     ID_n is Numbers +1,
     assert(node(ID_n,not(H),unprovable,[])),
     check_antecedants(T, NodeList).
+
+
+
 
 
 
